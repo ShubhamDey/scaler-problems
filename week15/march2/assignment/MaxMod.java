c3a98e710b53305dfcf7ff6a25466c724cf3a703
Shubham Dey <34033199+ShubhamDey@users.noreply.github.com>
2022-03-08 10:59:32 +0530

    Update MaxMod.java

--- week15/march2/assignment/MaxMod.java
+++ week15/march2/assignment/MaxMod.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /*
  * Given an array A of size N, groot wants you to pick 2 indices i and j such that
  * 1) 1 <= i, j <= N
- * @) A[i] % A[j] is maximum among all possible pairs of (i, j).
+ * 2) A[i] % A[j] is maximum among all possible pairs of (i, j).
  * Help Groot in finding the maximum value of A[i] % A[j] for some i, j.
  */
 
